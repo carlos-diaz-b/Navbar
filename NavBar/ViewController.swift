c117744a27fe3f1fe9213716911c93e51ddf4026
Carlos Diaz <ca.diaz@hotmail.com>
2020-05-15 12:25:13 -0700

    navbar.1

--- NavBar/ViewController.swift
+++ NavBar/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var NavBar: UINavigationBar!
+    
+    @IBAction func addButton(_ sender: Any) {
+        print("add button pressed")
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+       
+
     }
 
-
 }
-
